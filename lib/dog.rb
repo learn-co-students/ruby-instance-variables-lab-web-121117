ef7469032ec7aaf4b3ec4f0f6012f26be43bffc0
brianabaker <brianaclairbaker@gmail.com>
2017-12-13 16:41:14 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+
+class Dog
+
+  # def this_dogs_name(this_dogs_name)
+  #   this_dogs_name= this_dogs_name
+  # end
+
+  def name=(dog_name) #this makes an instance method
+    @this_dogs_name= dog_name
+  end
+
+  def name
+    @this_dogs_name
+  end
+
+end
